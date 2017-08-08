1775f4b4a5b0dc9747817d5c08a2c502381309d5
mhunsaker7 <mhunsaker7@gmail.com>
2017-08-07 20:20:35 -0700

    functionality complete

--- pyramid-slide.js
+++ pyramid-slide.js
@@ -1,10 +1,11 @@
+
 printPyramid(5);
+
 
 function printPyramid(height) {
     for (var counter = 1; counter < height + 1; counter++)
   console.log(" ".repeat(height-counter) + "#".repeat(counter + 1));
 }
-drawPyramid(5);
 
 function drawPyramid(height) {
     var node = document.getElementById("pyramid");
@@ -18,3 +19,26 @@ function drawPyramid(height) {
         node.appendChild(element);
     }
 }
+
+
+function newsymbol() {
+    var height = document.getElementById("heightInput").value;
+    var mylist=document.getElementById("symbol");
+    var symbol = mylist.options[mylist.selectedIndex].value;
+    var node = document.getElementById("pyramid");
+    
+    // Clear all children
+    while (node.firstChild) {
+        node.removeChild(node.firstChild);
+    }
+    
+    for (var counter = 1; counter < height +1; counter++){
+        var text = "\xa0".repeat(height-counter) + symbol.repeat(counter + 1)
+        var element = document.createElement("p");
+        element.textContent = text
+        node.appendChild(element);
+    }
+}
+
+
+window.onload = function () { newsymbol(); }
